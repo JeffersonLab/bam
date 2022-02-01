9dca8d5a3ce45de9a53e2b69b193f61df6de3b07
slominskir <ryan.slominski@gmail.com>
2022-02-01 15:19:43 -0500

    default data

---
+++ docker/orasetup/03_default_data.sql
@@ -0,0 +1,5 @@
+
+-- Populate Verification
+insert into BAM_OWNER.VERIFICATION (VERIFICATION_ID, NAME) values (1, 'Verified');
+insert into BAM_OWNER.VERIFICATION (VERIFICATION_ID, NAME) values (50, 'Provisionally Verified');
+insert into BAM_OWNER.VERIFICATION (VERIFICATION_ID, NAME) values (100, 'Not Verified');
